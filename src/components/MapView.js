98a02b512cb96def807fd5804e745b0f95a154e2
Hovhannes Matevosyan <h.matevosyan@yahoo.com>
2021-08-01 14:02:53 +0400

    main - added features to display list of placemarks on Yandex map API based on coordinates

--- src/components/MapView.js
+++ src/components/MapView.js
@@ -1,29 +1,42 @@
 import React, { Fragment, useState, useEffect } from 'react';
-import { YMaps, Map } from 'react-yandex-maps';
+import { YMaps, Map, Placemark } from 'react-yandex-maps';
 import axios from 'axios';
 
 function MapView(props) {
   const [lang, setLang] = useState('en_US');
+  const [countryData, setCountryData] = useState([]);
 
   const fetchData = async () => {
     const response = await axios.get('https://corona.lmao.ninja/v2/countries');
     return response.data;
   };
 
+  const coordinates = [
+    [55.611111, 37.738521],
+    [55.75, 37.573856],
+  ];
+
   useEffect(() => {
     setLang('en_US');
     fetchData()
       .then((res) => {
-        console.log('fetchData: ', res);
+        setCountryData(res);
       })
       .catch((err) => console.log('Could not get list of countries: ', err));
   }, []);
+
+  console.log('countryData  ***** ', countryData);
 
   return (
     <Fragment>
       <div className='map-view' id='map-view'>
         <YMaps key={{ lang: lang }} query={{ lang: lang }}>
-          <Map defaultState={{ center: [55.75, 37.57], zoom: 12 }} width='100%' height='100%' />
+          <Map defaultState={{ center: [55.75, 37.57], zoom: 12 }} width='100%' height='100%'>
+            {/* <Placemark geometry={[[55.1, 37.573856]]} />  */}
+            {coordinates.map((coordinate) => (
+              <Placemark geometry={coordinate} />
+            ))}
+          </Map>
         </YMaps>
       </div>
     </Fragment>
---
+++ src/interfaces/Country.tsx
@@ -0,0 +1,27 @@
+interface ICountry {
+  active: number;
+  activePerOneMillion: number;
+  cases: number;
+  casesPerOneMillion: number;
+  continent: string;
+  country: string;
+  countryInfo: { _id: number; iso2: string; iso3: string; lat: number; long: number };
+  critical: number;
+  criticalPerOneMillion: number;
+  deaths: number;
+  deathsPerOneMillion: number;
+  oneCasePerPeople: number;
+  oneDeathPerPeople: number;
+  oneTestPerPeople: number;
+  population: number;
+  recovered: number;
+  recoveredPerOneMillion: number;
+  tests: number;
+  testsPerOneMillion: number;
+  todayCases: number;
+  todayDeaths: number;
+  todayRecovered: number;
+  updated: number;
+}
+
+export default ICountry;
