f940199e03087ff440102faebb7adb976b0f9ea4
Hovhannes Matevosyan <h.matevosyan@yahoo.com>
2021-08-01 14:22:29 +0400

    main - added features to develop custom icons for placemarks in Yandex Map API

--- src/components/MapView.js
+++ src/components/MapView.js
@@ -12,7 +12,7 @@ function MapView(props) {
   };
 
   const coordinates = [
-    [55.611111, 37.738521],
+    [55.749, 37.738521],
     [55.75, 37.573856],
   ];
 
@@ -32,9 +32,23 @@ function MapView(props) {
       <div className='map-view' id='map-view'>
         <YMaps key={{ lang: lang }} query={{ lang: lang }}>
           <Map defaultState={{ center: [55.75, 37.57], zoom: 12 }} width='100%' height='100%'>
-            {/* <Placemark geometry={[[55.1, 37.573856]]} />  */}
-            {coordinates.map((coordinate) => (
-              <Placemark geometry={coordinate} />
+            {coordinates.map((coordinate, index) => (
+              <Placemark
+                geometry={coordinate}
+                key={index}
+                imageHref={'https://saguaroscuba.com/wp-content/uploads/2016/03/map-marker-icon.png'}
+                contentLayout='<div style="background: red; width: 50px; color: #FFFFFF; font-weight: bold;">Content of icon</div>'
+                options={{
+                  // Icon template type
+                  iconLayout: 'default#image',
+                  // Custom icon URL
+                  iconImageHref: 'https://saguaroscuba.com/wp-content/uploads/2016/03/map-marker-icon.png',
+                  // Icon size
+                  iconImageSize: [64, 64],
+                  // Icon offset position
+                  iconImageOffset: [-3, -42],
+                }}
+              />
             ))}
           </Map>
         </YMaps>
