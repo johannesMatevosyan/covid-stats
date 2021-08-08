b3c4949fd1feae66955c726425fe4196a89ca6d6
Hovhannes Matevosyan <h.matevosyan@yahoo.com>
2021-08-08 23:03:15 +0400

    main - added features to style total statistics boxes

--- src/components/TotalStatistics/TotalStatistics.js
+++ src/components/TotalStatistics/TotalStatistics.js
@@ -5,7 +5,38 @@ const TotalStatistics = (props) => {
   return (
     <>
       <section className={css.totalStatistics}>
-        <h2>TotalStatistics</h2>
+        <ul className={css.totalStatisticsList}>
+          <li className={css.statsTopRow}>
+            <div className={css.statNumberWrapper}>
+              <h2>Total Tests</h2>
+            </div>
+          </li>
+          <li className={css.statsTopRow}>
+            <div className={css.statNumberWrapper}>
+              <h2>Total Cases</h2>
+            </div>
+          </li>
+          <li className={css.statsTopRow}>
+            <div className={css.statNumberWrapper}>
+              <h2>Total Deaths</h2>
+            </div>
+          </li>
+          <li className={css.statsLowRow}>
+            <div className={css.statNumberWrapper}>
+              <h2>Active</h2>
+            </div>
+          </li>
+          <li className={css.statsLowRow}>
+            <div className={css.statNumberWrapper}>
+              <h2>Critical</h2>
+            </div>
+          </li>
+          <li className={css.statsLowRow}>
+            <div className={css.statNumberWrapper}>
+              <h2>Recovered</h2>
+            </div>
+          </li>
+        </ul>
       </section>
     </>
   );
